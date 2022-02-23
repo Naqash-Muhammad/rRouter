b7bcc4c09d8082c95d7258cb67c76d14f2eabdbe
Naqash <naqashmuhammad672@gmail.com>
2022-02-22 21:51:04 -0700

    mycommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './RouterB/Home';
+import About from './RouterB/About';
+import Context from './RouterB/Context';
+import {Switch,Route,Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <nav>
+        <ul>
+          <li>
+            <Link exact to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link exact to="/context">Context</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/context" component={Context} />
+      </Switch>
+      
+    </> 
   );
 }
 
---
+++ src/RouterB/Context.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Context = () => {
+  return (
+    <div>
+        <h1>this is a Context pzge</h1>
+    </div>
+  )
+}
+
+export default Context;
